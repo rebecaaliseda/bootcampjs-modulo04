67c2fd26336da1ea40df4431b1eb342a195b6454
Rebeca Aliseda <rebecaaliseda26@gmail.com>
2024-04-23 19:53:24 +0200

    Mejoras Laboratorio Modulo 4

--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,67 @@
 import './style.css';
 
 const botonSiguiente = document.getElementById('siguiente');
-var idNumTurno = document.getElementById('numeroTurno');
-if (idNumTurno !== null && idNumTurno !== undefined) {
-  var numeroTurno: number = parseInt(idNumTurno.innerHTML);
-}
-if (botonSiguiente !== null && botonSiguiente !== undefined) {
+const idNumTurno = document.getElementById('numeroTurno');
+let contador: number = 0;
+
+if (botonSiguiente && botonSiguiente instanceof HTMLButtonElement) {
   botonSiguiente.addEventListener('click', sumarNum);
 }
 function sumarNum() {
-  var result: number = ++numeroTurno;
-
-  var cadenaResult: string = result.toString();
+  let result: number = ++contador;
+  let cadenaResult: string = result.toString();
   if (result >= 0) {
     cadenaResult = cadenaResult.padStart(2, '0');
-    if (idNumTurno !== null && idNumTurno !== undefined) {
-      numeroTurno = result;
+    if (idNumTurno) {
+      contador = result;
       idNumTurno.innerHTML = cadenaResult;
     }
   }
 }
 
 const botonAnterior = document.getElementById('anterior');
-if (botonAnterior !== null && botonAnterior !== undefined) {
+if (botonAnterior && botonAnterior instanceof HTMLButtonElement) {
   botonAnterior.addEventListener('click', restarNum);
 }
 function restarNum() {
-  var result: number = --numeroTurno;
+  let result: number = --contador;
   if (result >= 0) {
-    var cadenaResult: string = result.toString();
+    let cadenaResult: string = result.toString();
     cadenaResult = cadenaResult.padStart(2, '0');
-    if (idNumTurno !== null && idNumTurno !== undefined) {
-      numeroTurno = result;
+    if (idNumTurno) {
+      contador = result;
       idNumTurno.innerHTML = cadenaResult;
     }
   }
 }
 
 const botonReset = document.getElementById('reset');
-if (botonReset !== null && botonReset !== undefined) {
+if (botonReset && botonReset instanceof HTMLButtonElement) {
   botonReset.addEventListener('click', resetNum);
 }
 function resetNum() {
-  var result: number = 0;
-  var cadenaResult: string = result.toString();
+  let result: number = 0;
+  let cadenaResult: string = result.toString();
   cadenaResult = cadenaResult.padStart(2, '0');
-  if (idNumTurno !== null && idNumTurno !== undefined) {
-    numeroTurno = result;
+  if (idNumTurno) {
+    contador = result;
     idNumTurno.innerHTML = cadenaResult;
   }
 }
 
 const botonActualizar = document.getElementById('actualizar');
-if (botonActualizar !== null && botonActualizar !== undefined) {
+if (botonActualizar && botonActualizar instanceof HTMLButtonElement) {
   botonActualizar.addEventListener('click', actualizarNum);
 }
 function actualizarNum() {
-  var introducirTurno = document.getElementById('introducirTurno');
-  if (introducirTurno !== null && introducirTurno !== undefined) {
-    var numTurnoIntroducido = (<HTMLInputElement>introducirTurno).value;
-    if (numTurnoIntroducido != null && numTurnoIntroducido != undefined) {
-      numeroTurno = parseInt(numTurnoIntroducido);
-      var cadenaNumTurno: string = numTurnoIntroducido.toString();
+  const introducirTurno = document.getElementById('introducirTurno');
+  if (introducirTurno && introducirTurno instanceof HTMLInputElement) {
+    let numTurnoIntroducido = introducirTurno.value;
+    if (numTurnoIntroducido) {
+      contador = parseInt(numTurnoIntroducido);
+      let cadenaNumTurno: string = numTurnoIntroducido.toString();
       cadenaNumTurno = cadenaNumTurno.padStart(2, '0');
-      if (idNumTurno !== null && idNumTurno !== undefined) {
+      if (idNumTurno) {
         idNumTurno.innerHTML = cadenaNumTurno;
       }
     }
